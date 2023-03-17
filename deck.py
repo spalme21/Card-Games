8693ad65f23196637361cb71c6e68738d849633b
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-03-17 19:25:44 +0000

    Add a method to shuffle the deck

--- deck.py
+++ deck.py
@@ -1,4 +1,5 @@
 from card import Card
+from random import shuffle
 
 class Deck:
     """A class to represent a deck of cards"""
@@ -8,4 +9,7 @@ class Deck:
         values = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 
         self.cards = [Card(suit, value) for suit in suits for value in values]
-                
+
+    def shuffle_deck(self):
+        """Return a shuffled deck"""
+        shuffle(self.cards)
