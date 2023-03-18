acd992b8e8c153e86b0040c5b086b03cb6b7128a
spalme21 <spalme21@gmail.com>
2023-03-18 16:39:01 -0500

    Update intro method

--- go_fish.py
+++ go_fish.py
@@ -1,3 +1,5 @@
+import time
+
 from deck import Deck
 
 class GoFish:
@@ -7,7 +9,21 @@ class GoFish:
         self.deck = Deck()
         self.computer_hand, self.player_hand = self.deck.deal_hands(2, 7)
 
-    def intro(self):
+    def run_game(self):
+        """Run the game"""
+        self._intro()
+
+    def _intro(self):
         """Introduce the game and the rules"""
         print("Welcome to Go Fish!")
+        time.sleep(3)
+        name = input("Enter your name: ")
+        print(f"Hello {name}!")
+        time.sleep(1)
+        print("Here's your hand:")
+        print(self.player_hand)
 
+
+if __name__ == "__main__":
+    game = GoFish()
+    game.run_game()
